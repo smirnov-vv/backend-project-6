ec7ebf4942d5ea23da6b0999c4ac3371a8475385
smirnov-vv <vsmirnov.pro@gmail.com>
2023-05-06 11:44:54 +0300

    refactor: change solution of updating updated_at field

--- server/models/BaseModel.cjs
+++ server/models/BaseModel.cjs
@@ -3,6 +3,10 @@
 const { Model } = require('objection');
 
 module.exports = class BaseModel extends Model {
+  $beforeUpdate() {
+    // this.updated_at = knex.fn.now();
+    this.updated_at = new Date().toISOString();
+  }
   static get modelPaths() {
     return [__dirname];
   }
