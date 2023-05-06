8c2d1fb08431f5ce690b65e70e34d5335e637a9d
smirnov-vv <vsmirnov.pro@gmail.com>
2023-05-06 11:43:44 +0300

    feat: add timezone

--- server/migrations/20230427163705_create_tags_table.js
+++ server/migrations/20230427163705_create_tags_table.js
@@ -4,8 +4,8 @@ export const up = (knex) => (
   knex.schema.createTable('tags', (table) => {
     table.increments('id').primary();
     table.string('name');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.timestamp('created_at', { useTz: true }).defaultTo(knex.fn.now());
+    table.timestamp('updated_at', { useTz: true }).defaultTo(knex.fn.now());
   })
 );
 
