69e412c579eff0fe557ec639e3df8c2b022a2d7f
smirnov-vv <vsmirnov.pro@gmail.com>
2023-05-16 20:43:47 +0300

    feat: change languages

--- server/routes/welcome.js
+++ server/routes/welcome.js
@@ -1,9 +1,14 @@
 // @ts-check
+import i18next from 'i18next';
 
 export default (app) => {
   app
     .get('/', { name: 'root' }, (req, reply) => {
       reply.render('welcome/index');
+    })
+    .get('/change', (req, reply) => {
+      i18next.changeLanguage(req.query.lang);
+      reply.redirect(req.headers.referer);
     })
     .get('/protected', { name: 'protected', preValidation: app.authenticate }, (req, reply) => {
       reply.render('welcome/index');
