20b020ba687453b572fe2160c706cc5d8dd47b0e
smirnov-vv <vsmirnov.pro@gmail.com>
2023-05-22 18:44:02 +0300

    refactor: reduce time to fix codeclimate issue

--- server/routes/statuses.js
+++ server/routes/statuses.js
@@ -3,11 +3,10 @@
 import * as routes from './routes.js';
 
 export default (app) => {
-  app
-    .get('/statuses', { name: 'statuses', preValidation: app.authenticate }, (req, reply) => routes.get(req, reply, app, 'statuses'))
-    .get('/statuses/new', { name: 'newStatus', preValidation: app.authenticate }, (req, reply) => routes.getNew(req, reply, app, 'statuses'))
-    .get('/statuses/:id/edit', { name: 'editStatus', preValidation: app.authenticate }, (req, reply) => routes.edit(req, reply, app, 'statuses'))
-    .patch('/statuses/:id', { name: 'patchStatus', preValidation: app.authenticate }, (req, reply) => routes.patch(req, reply, app, 'statuses'))
-    .delete('/statuses/:id', { name: 'deleteStatus', preValidation: app.authenticate }, (req, reply) => routes.del(req, reply, app, 'statuses', 'task', 'statusId'))
-    .post('/statuses', { preValidation: app.authenticate }, (req, reply) => routes.post(req, reply, app, 'statuses'));
+  app.get('/statuses', { name: 'statuses', preValidation: app.authenticate }, (req, reply) => routes.get(req, reply, app, 'statuses'));
+  app.get('/statuses/new', { name: 'newStatus', preValidation: app.authenticate }, (req, reply) => routes.getNew(req, reply, app, 'statuses'));
+  app.get('/statuses/:id/edit', { name: 'editStatus', preValidation: app.authenticate }, (req, reply) => routes.edit(req, reply, app, 'statuses'));
+  app.patch('/statuses/:id', { name: 'patchStatus', preValidation: app.authenticate }, (req, reply) => routes.patch(req, reply, app, 'statuses'));
+  app.delete('/statuses/:id', { name: 'deleteStatus', preValidation: app.authenticate }, (req, reply) => routes.del(req, reply, app, 'statuses', 'task', 'statusId'));
+  app.post('/statuses', { preValidation: app.authenticate }, (req, reply) => routes.post(req, reply, app, 'statuses'));
 };
--- server/routes/tags.js
+++ server/routes/tags.js
@@ -3,11 +3,10 @@
 import * as routes from './routes.js';
 
 export default (app) => {
-  app
-    .get('/labels', { name: 'tags', preValidation: app.authenticate }, (req, reply) => routes.get(req, reply, app, 'labels'))
-    .get('/labels/new', { name: 'newTag', preValidation: app.authenticate }, (req, reply) => routes.getNew(req, reply, app, 'labels'))
-    .get('/labels/:id/edit', { name: 'editTag', preValidation: app.authenticate }, (req, reply) => routes.edit(req, reply, app, 'labels'))
-    .patch('/labels/:id', { name: 'patchTag', preValidation: app.authenticate }, (req, reply) => routes.patch(req, reply, app, 'labels'))
-    .delete('/labels/:id', { name: 'deleteTag', preValidation: app.authenticate }, (req, reply) => routes.del(req, reply, app, 'labels', 'taskTag', 'tagId'))
-    .post('/labels', { name: 'labels', preValidation: app.authenticate }, (req, reply) => routes.post(req, reply, app, 'labels'));
+  app.get('/labels', { name: 'tags', preValidation: app.authenticate }, (req, reply) => routes.get(req, reply, app, 'labels'));
+  app.get('/labels/new', { name: 'newTag', preValidation: app.authenticate }, (req, reply) => routes.getNew(req, reply, app, 'labels'));
+  app.get('/labels/:id/edit', { name: 'editTag', preValidation: app.authenticate }, (req, reply) => routes.edit(req, reply, app, 'labels'));
+  app.patch('/labels/:id', { name: 'patchTag', preValidation: app.authenticate }, (req, reply) => routes.patch(req, reply, app, 'labels'));
+  app.delete('/labels/:id', { name: 'deleteTag', preValidation: app.authenticate }, (req, reply) => routes.del(req, reply, app, 'labels', 'taskTag', 'tagId'));
+  app.post('/labels', { name: 'labels', preValidation: app.authenticate }, (req, reply) => routes.post(req, reply, app, 'labels'));
 };
